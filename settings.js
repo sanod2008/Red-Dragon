7a0dac894e4107d7348442dc7440519c10dc2d8b
sanod2008 <109646550+sanod2008@users.noreply.github.com>
2022-07-20 11:03:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,14 +20,14 @@ global.APIKeys = {
 //customize settings\\
 
 global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+My Creator is sanod yasiru` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94704003491'] //Owner number in aive msg
+global.premium = ['+94779174411'] //Owner Number info
+global.ownernomer = '+94779174411' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'Sanod yasiru' //Owner Name
+global.botname = ' SY Geek bot ' //Bot Name
 
 global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
